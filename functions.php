d1e3ca14cbab31b2c9cf16ee83728c6d32ac0c30
Jigme Datse Yli-Rasku <jigme.datse@datsemultimedia.com>
2016-06-30 16:21:43 -0700

    Base of Child Theme setup
    The child theme with the files `fuctions.php` and `style.css` has been
    added.
    
    This should help.

---
+++ functions.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * Created on Aug 3, 2015
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+
+function theme_enqueue_styles() {
+
+	$parent_style = 'datse-multimedia-base';
+
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style',
+			get_stylesheet_directory_uri() . '/style.css',
+			array( $parent_style )
+	);
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
+//add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+//add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+//function my_theme_wrapper_start() {
+//  echo '<section id="main">';
+//}
+
+//function my_theme_wrapper_end() {
+//  echo '</section>';
+//}
+
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+
+?>
